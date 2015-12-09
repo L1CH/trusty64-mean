54d5b138007e1340a8ac5e3fb98155c789ce6619
Qiyu Li <i@qiyu.li>
2015-12-08 19:13:53 -0500

    Change xvfb's format to unix

--- .provision/xvfb.init.sh
+++ .provision/xvfb.init.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-sudo apt-get install -y xvfb
+sudo apt-get install -y xvfb dos2unix
 # Setup XVFB
 if ! [ -e /etc/init.d/xvfb ]; then
   echo 'File /etc/init.d/xvfb is generating...'
   sudo cp ~/xvfb /etc/init.d/
   sudo rm -rf ~/xvfb
   sudo chown root:root /etc/init.d/xvfb
-  sudo chmod 744 /etc/init.d/xvfb
+  sudo chmod 755 /etc/init.d/xvfb
+  sudo dos2unix /etc/init.d/xvfb
   sudo update-rc.d xvfb defaults
   echo '' | tee >> ~/.bashrc
   echo 'export DISPLAY=:99' | tee >> ~/.bashrc
